b134d20f63f3d20d5319bfd512addab69ac6b17f
AlexandreMohr <33735538+AlexandreMohr@users.noreply.github.com>
2017-11-16 19:54:44 -0200

    Add files via upload

---
+++ Códigos em JAVA/Registros/RegistroDeColega.java
@@ -0,0 +1,35 @@
+package Registros;
+
+import java.util.ArrayList;
+
+import Dados.Colega;
+
+public class RegistroDeColega {
+	
+	private ArrayList<Colega> rC;
+
+	public RegistroDeColega() {
+		rC = new ArrayList<Colega>();
+	}
+	
+	public void addColega (Colega colega) {
+		rC.add(colega);
+	}
+
+	public Colega getColega (int indice) {
+		return rC.get(indice);
+	}
+	
+	public void removeContato (Colega colega) {
+		rC.remove(colega);
+	}
+	
+	public void removeContato (int indice) {
+		rC.remove(indice);
+	}
+	
+	public int size() {
+		return rC.size();
+	}
+
+}
